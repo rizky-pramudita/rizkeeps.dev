a9c0ad2783e8a97586ab6ad070e984e22c06bc92
rizky-pramudita <rzkprmdt@gmail.com>
2023-05-08 15:17:59 +0700

    home

--- src/components/ProfileHome.js
+++ src/components/ProfileHome.js
@@ -1,34 +1,33 @@
-import React from 'react'
+ import React from 'react'
 
 const ProfileHome = () => {
   return (
-    <div className='rounded-lg flex flex-row justify-between m-5'>
-        <div className='bg-iris rounded-lg w-[50%] md:h-[22rem] h-[15rem] flex items-center justify-center'>
-            <img src='https://res.cloudinary.com/dg4b8sell/image/upload/v1679290175/DSCF5352-removebg-preview_v5qpub.png' alt='Rizky Pramudita' className='bg-cover object-cover bg-center h-full w-full'/>
+    <div className='rounded-lg flex flex-col md:flex-row justify-between m-5 gap-2'>
+        <div className='bg-iris rounded-lg md:w-[50%] md:h-[22rem] h-[15rem] flex items-center justify-center'>
+            <img src='https://storage.googleapis.com/rizkeeps-dev/rizkeeps.dev-profile' alt='Rizky Pramudita' className='bg-cover object-cover bg-center h-full w-full'/>
         </div>
-          <div className='flex flex-col gap-4'>
-            <div className='bg-grey-card rounded-lg flex flex-col p-4 gap-5 ml-[20px]'>
+          <div className='flex flex-col justify-between gap-4'>
+            <div className='bg-grey-card rounded-lg flex flex-col p-4 gap-5 md:ml-[20px]'>
                 <h1 className='text-white font-regular md:text-2xl text-md'>Rizky Pramudita Setyawan</h1>
                 <h1>
-                    <span className='text-white font-semibold md:text-2xl sm:text-xl text-md'>Digital Product</span>
+                    <span className='text-white font-semibold md:text-2xl sm:text-xl text-md'>SOFTWARE ENGINEERING</span>
                     <span className='text-white font-semibold md:text-2xl sm:text-xl text-md'> Enthusiast</span>
                 </h1>
             </div>
-            <div className='grid grid-cols-4 md:py-1 md:px-1 py-2 px-2 bg-grey-card rounded-lg p-4 gap-5 ml-[20px]'>
-              <button className=''>
-                <img src='https://res.cloudinary.com/dg4b8sell/image/upload/v1680164096/rizkeeps/Vector_ehzki7.png' alt='Rizky-Github' className='bg-cover object-cover bg-center w-[35px]'/>
-              </button>
-              <button className=''>
-                <img src='https://res.cloudinary.com/dg4b8sell/image/upload/v1680164096/rizkeeps/Vector-1_spce0r.png' alt='Rizky-Github' className='bg-cover object-cover bg-center w-[35px]'/>
-              </button>
-              <button className=''>
-                <img src='https://res.cloudinary.com/dg4b8sell/image/upload/v1680164096/rizkeeps/Vector-2_jrr5dj.png' alt='Rizky-Github' className='bg-cover object-cover bg-center w-[35px]'/>
-              </button>
-              <button className=''>
-                <img src='https://res.cloudinary.com/dg4b8sell/image/upload/v1680164097/rizkeeps/Vector-3_nj1hc4.png' alt='Rizky-Github' className='bg-cover object-cover bg-center w-[35px]'/>
-              </button>
-            </div>
-          </div>
+            <div className='bg-grey-card flex flex-row p-4 gap-3 ml-[0px] md:ml-[20px] rounded-lg justify-between'>
+
+              <a href='https://github.com/rizky-pramudita' target='_blank' rel='noreferrer'><svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0,0,256,256" width="60px" height="60px" fill-rule="nonzero"><g fill="#ffffff" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none"><g transform="scale(8.53333,8.53333)"><path d="M15,3c-6.627,0 -12,5.373 -12,12c0,5.623 3.872,10.328 9.092,11.63c-0.056,-0.162 -0.092,-0.35 -0.092,-0.583v-2.051c-0.487,0 -1.303,0 -1.508,0c-0.821,0 -1.551,-0.353 -1.905,-1.009c-0.393,-0.729 -0.461,-1.844 -1.435,-2.526c-0.289,-0.227 -0.069,-0.486 0.264,-0.451c0.615,0.174 1.125,0.596 1.605,1.222c0.478,0.627 0.703,0.769 1.596,0.769c0.433,0 1.081,-0.025 1.691,-0.121c0.328,-0.833 0.895,-1.6 1.588,-1.962c-3.996,-0.411 -5.903,-2.399 -5.903,-5.098c0,-1.162 0.495,-2.286 1.336,-3.233c-0.276,-0.94 -0.623,-2.857 0.106,-3.587c1.798,0 2.885,1.166 3.146,1.481c0.896,-0.307 1.88,-0.481 2.914,-0.481c1.036,0 2.024,0.174 2.922,0.483c0.258,-0.313 1.346,-1.483 3.148,-1.483c0.732,0.731 0.381,2.656 0.102,3.594c0.836,0.945 1.328,2.066 1.328,3.226c0,2.697 -1.904,4.684 -5.894,5.097c1.098,0.573 1.899,2.183 1.899,3.396v2.734c0,0.104 -0.023,0.179 -0.035,0.268c4.676,-1.639 8.035,-6.079 8.035,-11.315c0,-6.627 -5.373,-12 -12,-12z"></path></g></g></svg>
+              </a>
+              <a href='https://www.youtube.com/channel/UCxxagiWe6lqcMjthOIAQYog' target='_blank' rel='noreferrer'><svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0,0,256,256" width="60px" height="60px" fill-rule="nonzero"><g fill="#ffffff" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none"><g transform="scale(8.53333,8.53333)"><path d="M15,4c-4.186,0 -9.61914,1.04883 -9.61914,1.04883l-0.01367,0.01563c-1.90652,0.30491 -3.36719,1.94317 -3.36719,3.93555v6v0.00195v5.99805v0.00195c0.00384,1.96564 1.4353,3.63719 3.37695,3.94336l0.00391,0.00586c0,0 5.43314,1.05078 9.61914,1.05078c4.186,0 9.61914,-1.05078 9.61914,-1.05078l0.00195,-0.00195c1.94389,-0.30554 3.37683,-1.97951 3.37891,-3.94727v-0.00195v-5.99805v-0.00195v-6c-0.00288,-1.96638 -1.43457,-3.63903 -3.37695,-3.94531l-0.00391,-0.00586c0,0 -5.43314,-1.04883 -9.61914,-1.04883zM12,10.39844l8,4.60156l-8,4.60156z"></path></g></g></svg>
+              </a>
+              <a href='https://www.instagram.com/rizkeeps' target='_blank' rel='noreferrer'>
+              <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0,0,256,256" width="60px" height="60px" fill-rule="nonzero"><g fill="#ffffff" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none"><g transform="scale(8.53333,8.53333)"><path d="M9.99805,3c-3.859,0 -6.99805,3.14195 -6.99805,7.00195v10c0,3.859 3.14195,6.99805 7.00195,6.99805h10c3.859,0 6.99805,-3.14195 6.99805,-7.00195v-10c0,-3.859 -3.14195,-6.99805 -7.00195,-6.99805zM22,7c0.552,0 1,0.448 1,1c0,0.552 -0.448,1 -1,1c-0.552,0 -1,-0.448 -1,-1c0,-0.552 0.448,-1 1,-1zM15,9c3.309,0 6,2.691 6,6c0,3.309 -2.691,6 -6,6c-3.309,0 -6,-2.691 -6,-6c0,-3.309 2.691,-6 6,-6zM15,11c-2.20914,0 -4,1.79086 -4,4c0,2.20914 1.79086,4 4,4c2.20914,0 4,-1.79086 4,-4c0,-2.20914 -1.79086,-4 -4,-4z"></path></g></g></svg>
+              </a>
+              <a href='https://www.linkedin.com/in/rizky-pramudita-3048511a0/' target='_blank' rel='noreferrer'>
+              <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0,0,256,256" width="60px" height="60px" fill-rule="nonzero"><g fill="#ffffff" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none"><g transform="scale(8.53333,8.53333)"><path d="M24,4h-18c-1.105,0 -2,0.895 -2,2v18c0,1.105 0.895,2 2,2h18c1.105,0 2,-0.895 2,-2v-18c0,-1.105 -0.895,-2 -2,-2zM10.954,22h-2.95v-9.492h2.95zM9.449,11.151c-0.951,0 -1.72,-0.771 -1.72,-1.72c0,-0.949 0.77,-1.719 1.72,-1.719c0.948,0 1.719,0.771 1.719,1.719c0,0.949 -0.771,1.72 -1.719,1.72zM22.004,22h-2.948v-4.616c0,-1.101 -0.02,-2.517 -1.533,-2.517c-1.535,0 -1.771,1.199 -1.771,2.437v4.696h-2.948v-9.492h2.83v1.297h0.04c0.394,-0.746 1.356,-1.533 2.791,-1.533c2.987,0 3.539,1.966 3.539,4.522z"></path></g></g></svg>
+              </a>
+              </div>
+              </div>
     </div>
   )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,22 +1,16 @@
 import React from 'react'
 import Quote from '../components/Quote'
-import ProjectCount from '../components/ProjectCount'
-import ProjectShelf from '../components/ProjectShelf'
 import HeaderHome from '../components/HeaderHome'
 import ProfileHome from '../components/ProfileHome'
 import AboutMe from '../components/AboutMe'
 
 export default function Home() {
   return (
-    <div className='flex md:flex-row flex-col-reverse '>
-      <div className='flex-grow-1 md:w-[50%] w-[100%]'>
-        <Quote></Quote>
-        <AboutMe></AboutMe>
-      </div>
-      <div className='flex-grow-1 md:w-[50%] w-[100%]'>
-        <HeaderHome></HeaderHome>
-        <ProfileHome></ProfileHome>
-      </div>
+    <div className='flex flex-col md:w-[50%] mx-auto'>
+      <HeaderHome></HeaderHome>
+      <ProfileHome></ProfileHome>
+      <Quote></Quote>
+      <AboutMe></AboutMe>
     </div>
   )
 }
